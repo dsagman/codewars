599b9c4cbffd1db43d4305fa6f879205ac8a48b8
dsagman <70487040+dsagman@users.noreply.github.com>
2023-06-10 22:54:37 -0500

    some more kata

--- typescript/pangram.ts
+++ typescript/pangram.ts
@@ -1,1 +1,21 @@
-export const isPangram = (phrase: string): boolean => true
+// export const isPangram = (phrase: string): boolean => {
+
+//     const alphabet = "abcdefghijklmnopqrstuvwxyz";
+//     const phraseLower = phrase.toLowerCase();
+//     for (let i = 0; i < alphabet.length; i++) {
+//         if (phraseLower.indexOf(alphabet.charAt(i)) === -1) {
+//             return false;
+//         }
+//     }
+//     return true;
+// };
+
+export const isPangram = (phrase: string): boolean => {
+    return new Set(phrase.toLowerCase()
+        .replace(/[^a-z]/gi, ''))
+        .size === 26;
+};
+// [...new Set(phrase.toLowerCase()] makes a list of unique characters
+
+console.log(isPangram("The quick brown fox jumps over the lazy dog."), true);
+console.log(isPangram("This is not a pangram."), false);
