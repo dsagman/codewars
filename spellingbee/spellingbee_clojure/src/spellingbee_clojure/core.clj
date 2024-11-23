6b97bed451f76b87f474d4aa9b4f4ccbab1e0a75
dsagman <70487040+dsagman@users.noreply.github.com>
2024-11-22 21:53:05 -0600

    clojure project

--- spellingbee/spellingbee_clojure/src/spellingbee_clojure/core.clj
+++ spellingbee/spellingbee_clojure/src/spellingbee_clojure/core.clj
@@ -1,7 +1,15 @@
 (ns spellingbee-clojure.core
   (:gen-class))
 
+(defn -read-dictionary
+  "Reads the dictionary file and returns a list of words."
+  []
+  (let [dictionary-file "/usr/share/dict/words"]
+    (with-open [rdr (clojure.java.io/reader dictionary-file)]
+      (doall (line-seq rdr)))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+  (println (-read-dictionary))
